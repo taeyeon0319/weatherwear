abfb7d52eac0bdc60b30d9d1d132d29eeb61cd07
taeyeon0319 <xodus1349@likelion.org>
2021-07-02 15:54:42 +0900

    댓글 CRUD

--- weather_wear/community/models.py
+++ weather_wear/community/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Community(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    writer = models.CharField(max_length=100)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="community/", blank=True, null=True)
@@ -13,3 +14,11 @@ class Community(models.Model):
 
     def summary(self):
         return self.body[:20]
+
+class Comment(models.Model):
+    content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- weather_wear/community/views.py
+++ weather_wear/community/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Community
+from .models import Community, Comment
 from django.utils import timezone
 
 def show(request):
@@ -8,7 +8,8 @@ def show(request):
 
 def detail(request, id):
     community = get_object_or_404(Community, id = id) 
-    return render(request, 'community/detail.html', {'community':community})
+    all_comments = community.comments.order_by('-created_at')
+    return render(request, 'community/detail.html', {'community':community, 'comments':all_comments})
 
 def new(request):
     return render(request, 'community/new.html')    
@@ -16,7 +17,7 @@ def new(request):
 def create(request):
     new_community = Community()
     new_community.title = request.POST['title']
-    new_community.writer = request.POST['writer']
+    new_community.writer = request.user
     new_community.pub_date = timezone.now()
     new_community.body = request.POST['body']
     new_community.image = request.FILES.get('image')
@@ -30,7 +31,7 @@ def edit(request, id):
 def update(request, id):
     update_community = Community.objects.get(id = id)
     update_community.title = request.POST['title']
-    update_community.writer = request.POST['writer']
+    update_community.writer = request.user
     update_community.pub_date = timezone.now()
     update_community.body = request.POST['body']
     update_community.image = request.FILES.get('image')
@@ -42,3 +43,25 @@ def delete(request, id):
     delete_community.delete()
     return redirect('community:show')
 
+def create_comment(request, community_id):
+    if request.method == "POST":
+        community = get_object_or_404(Community, pk=community_id)
+        current_user = request.user
+        comment_content = request.POST.get('content')
+        Comment.objects.create(content=comment_content, writer=current_user, community=community)
+    return redirect('community:detail', community_id)
+
+def update_comment(request, comment_id):
+    comment=get_object_or_404(Comment, pk=comment_id)
+    if request.method == "POST":
+        community_id = comment.community.id
+        comment.content=request.POST.get('content')
+        comment.save()
+        return redirect('community:detail', community_id)
+    return render(request, 'community/update_comment.html',{"comment":comment})
+
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    community_id = comment.community.id
+    comment.delete()
+    return redirect('community:detail', community_id)
