bd5d23fd0392abefc65cfd7a63df7ea59e529247
minju <alswn9938@gmail.com>
2021-07-04 00:33:44 +0900

    [2021.07.04 정민주] 착장페이지 기초 구현

--- weather_wear/myPage/models.py
+++ weather_wear/myPage/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+import sys
+sys.path.append("..")
+from django.conf import settings
 
 # Create your models here.
+
+
+#자기 착장 기록
+
+class MyClothes(models.Model):
+    id=models.AutoField(primary_key=True)
+    #제목
+    title=models.CharField(max_length=300)
+    #날씨 카테고리
+    WEATHER_CHOICE=(
+        ('맑음','맑음'),
+        ('흐림','흐림'),
+        ('비','비'),
+        ('눈','눈'),
+    )
+    weather=models.CharField(max_length=20, choices=WEATHER_CHOICE, default="")
+    #기온
+    temperature=models.IntegerField(default=0)
+    #날짜
+    post_date=models.DateField()
+    #작성자
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="작성자", default="")
+    thumbnail=models.ImageField(upload_to='images', blank=True, null=True, default="{%static 'images/blank.png' %}")
+    memo=models.TextField(default="")
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table="MyClothes"
--- weather_wear/myPage/views.py
+++ weather_wear/myPage/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import redirect, render
+from .models import MyClothes
+from django.shortcuts import get_object_or_404, redirect, render
 import sys
 sys.path.append("..")
 from accounts.models import UserProfile
 from community.models import Community
 from django.core.paginator import Paginator
+from datetime import datetime
 # Create your views here.
 
 #마이페이지로 이동하는 함수
@@ -66,3 +68,59 @@ def filter_list(request):
     paginator = Paginator(community,3)
     boards = paginator.get_page(page)
     return render(request,'myPage/writing_list.html',{'boards':boards, 'count':count,'selected_weather':selected_weather })
+#자기착장페이지로 이동
+def my_clothes(request):
+    user=request.user
+    clothes=MyClothes.objects.filter(user=user).order_by('-id')
+    check=request.user.email
+    profiles=UserProfile.objects.get(email=check)
+    page=int(request.GET.get('p',1))
+    paginator=Paginator(clothes, 3)
+    boards=paginator.get_page(page)
+    return render(request, 'myPage/my_clothes.html',{'profiles':profiles, 'clothes':clothes, 'boards':boards})
+
+#글쓰기
+def new_clothes(request):
+    check=request.user.email
+    profiles=UserProfile.objects.get(email=check)
+    return render(request, 'myPage/new_clothes.html',{'profiles':profiles})
+
+#착장만들기
+def create_clothes(request):
+    new_clothes=MyClothes()
+    new_clothes.title=request.POST['title']
+    new_clothes.weather=request.POST['category_radio']
+    new_clothes.temperature=request.POST['temperature']
+    new_clothes.post_date=datetime.today()
+    new_clothes.thumbnail=request.FILES.get('thumbnail')
+    new_clothes.user=request.user
+    new_clothes.memo=request.POST['memo']
+    new_clothes.save()
+    return redirect('myPage:my_clothes')
+
+def detail_clothes(request, id):
+    clothes=get_object_or_404(MyClothes,pk=id)
+    return render(request, 'myPage/detail_clothes.html',{'clothes':clothes})
+
+ 
+def delete_clothes(request,id):
+    delete=MyClothes.objects.get(id=id)
+    delete.delete()
+    return redirect('myPage:my_clothes')
+
+
+def edit_clothes(request,id):
+    edit=MyClothes.objects.get(id=id)
+    return render(request, 'myPage/edit_clothes.html',{'clothes':edit})
+
+def update_clothes(request, id):
+    update_clothes=MyClothes()
+    update_clothes.title=request.POST['title']
+    update_clothes.weather=request.POST['category_radio']
+    update_clothes.temperature=request.POST['temperature']
+    update_clothes.post_date=datetime.today()
+    update_clothes.thumbnail=request.FILES.get('thumbnail')
+    update_clothes.user=request.user
+    update_clothes.memo=request.POST['memo']
+    update_clothes.save()
+    return redirect('myPage:detail_clothes',update_clothes.id)
