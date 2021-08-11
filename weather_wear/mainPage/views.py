3a4b69530f42bc2411650a70c0ee5247c918a658
hwan0303 <asok369258@naver.com>
2021-08-11 17:10:29 +0900

    메인 페이지 수정 및 옷 정보 추가

--- weather_wear/mainPage/views.py
+++ weather_wear/mainPage/views.py
@@ -28,9 +28,43 @@ def today(request):
     result['humidity'] = soup.find("span", {"id": "wob_hm"}).text
     # 7. 풍속
     result['wind'] = soup.find("span", {"id": "wob_ws"}).text
+    temp_int=int(result['temp_now'])
+    precipitation_int=int(result['precipitation'].rstrip('%'))
+    humidity_int=int(result['humidity'].rstrip('%'))
+    wind_float=float(result['wind'].rstrip('m/s'))
+    
+    return render(request, 'mainPage/today.html',{'weather': result , 'temp_int' : temp_int , 'precipitation_int': precipitation_int , 'humidity_int': humidity_int , 'wind_float': wind_float})
+ 
 
-
-    return render(request, 'mainPage/today.html', {'weather': result}) 
 
 def mainpage(request):
-    return render(request, 'mainPage/mainpage.html')
+     # 지역은 서울로 고정
+    city = 'location'
+    # 서울 지역의 날씨 정보 불러오기
+    URL = f'https://www.google.com/search?q=weather+{city}'
+    # 헤더 설정
+    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36 OPR/67.0.3575.115'}
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser', from_encoding="utf8")
+    result = dict()
+
+    # 가져올 데이터 세팅
+    # 1. 지역
+    result['region'] = soup.find("div", {"id": "wob_loc"}).text
+    # 2. 시간
+    result['dayhour'] = soup.find("div", {"id": "wob_dts"}).text
+    # 3. 기온
+    result['temp_now'] = soup.find("span", {"id": "wob_tm"}).text
+    # 4. 날씨
+    result['weather_now'] = soup.find("span", {"id": "wob_dc"}).text
+    # 5. 강수
+    result['precipitation'] = soup.find("span", {"id": "wob_pp"}).text
+    # 6. 습도
+    result['humidity'] = soup.find("span", {"id": "wob_hm"}).text
+    # 7. 풍속
+    result['wind'] = soup.find("span", {"id": "wob_ws"}).text
+    temp_int=int(result['temp_now'])
+    precipitation_int=int(result['precipitation'].rstrip('%'))
+    humidity_int=int(result['humidity'].rstrip('%'))
+    wind_float=float(result['wind'].rstrip('m/s'))
+    return render(request, 'mainPage/mainPage.html',{'weather': result })
